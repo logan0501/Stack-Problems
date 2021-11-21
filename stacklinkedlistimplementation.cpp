ff6bcd5a855a3d8a2592227638267044f77fe2cd
Loganathan <loganvk18@gmail.com>
2021-11-21 20:03:03 +0530

    completed ll implementation

--- stacklinkedlistimplementation.cpp
+++ stacklinkedlistimplementation.cpp
@@ -1,5 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* top;
+int isEmpty(){
+    return top==NULL;
+}
+void push(int x){
+    Node* temp=new Node();
+    temp->data=x;
+    temp->next=top;
+    top=temp;
+}
+void pop(){
+    Node* temp;
+    if(!isEmpty())return;
+    temp=top;
+    top=top->next;
+    temp->next=NULL;
+    delete temp;
+}
+int peek(){
+    if(isEmpty())
+    return top->data;
+    return -1;
+}
 
 int main(){
 
